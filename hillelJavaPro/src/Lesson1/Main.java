e777ba2796c9fe30b62ddf88bb48b9bc0cbf1ae0
AluslayerCard <91783508+AluslayerCard@users.noreply.github.com>
2022-12-11 22:52:56 +0200

    Update Main.java

--- hillelJavaPro/src/Lesson1/Main.java
+++ hillelJavaPro/src/Lesson1/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("TR--");
         
-        System.out.println("conflict git");
+        System.out.println("conflict git ");
     }
 }
