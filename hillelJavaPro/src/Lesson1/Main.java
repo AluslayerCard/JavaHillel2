bc018d6cbe1cfc11d6cb6133867cdab3689ec8bc
AluslayerCard <91783508+AluslayerCard@users.noreply.github.com>
2022-12-11 22:57:20 +0200

    conflict commit

--- hillelJavaPro/src/Lesson1/Main.java
+++ hillelJavaPro/src/Lesson1/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("TR--");
         System.out.println("conflict");
 
-        System.out.println(" squash ");
+        System.out.println("squash");
         
         
     }
