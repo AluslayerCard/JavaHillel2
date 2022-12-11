29434fa622da4487d06a3eb07a29c8a115b4b1bf
AluslayerCard <91783508+AluslayerCard@users.noreply.github.com>
2022-12-11 22:33:18 +0200

    conflict commit

--- hillelJavaPro/src/Lesson1/Main.java
+++ hillelJavaPro/src/Lesson1/Main.java
@@ -3,7 +3,6 @@ package Lesson1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("TR--");
-        
-        System.out.println("conflict by git");
+        System.out.println("conflict");
     }
 }
